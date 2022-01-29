a30f6853624c29430577923def9800f56ad49345
deepso <deeps.o7o8.y@gmail.com>
2022-01-30 03:17:10 +0530

    display users home page

--- apps/web-react/src/pages/Home.tsx
+++ apps/web-react/src/pages/Home.tsx
@@ -1,9 +1,25 @@
 import { FC } from "react";
+import { useUsersQuery } from "../generated/graphql";
 
 interface HomeProps {}
 
 const Home: FC<HomeProps> = ({}) => {
-  return <div>Home</div>;
+  const { data } = useUsersQuery({ fetchPolicy: "network-only" });
+
+  if (!data) return <div>Loading...</div>;
+
+  return (
+    <div>
+      <div>Users:</div>
+      <ul>
+        {data.users.map(user => (
+          <li key={user.id}>
+            {user.email}, {user.id}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 };
 
 export default Home;
