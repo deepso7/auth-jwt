cfde5b759427bf18e13882d3578147102d4d8dc3
deepso <deeps.o7o8.y@gmail.com>
2022-01-30 03:12:14 +0530

    add register logic

--- apps/web-react/src/pages/Register.tsx
+++ apps/web-react/src/pages/Register.tsx
@@ -1,14 +1,29 @@
 import { FC, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { useRegisterMutation } from "../generated/graphql";
 
 interface RegisterProps {}
 
 const Register: FC<RegisterProps> = ({}) => {
+  const navigate = useNavigate();
+
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [register] = useRegisterMutation();
 
-  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
     console.log({ email, password });
+    const res = await register({
+      variables: {
+        email,
+        password,
+      },
+    });
+
+    console.log({ res });
+
+    navigate("/");
   };
 
   return (
