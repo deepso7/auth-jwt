17d430fe7139d6d0c79c8c904b5fe862a1e8d5e5
deepso <deeps.o7o8.y@gmail.com>
2022-01-30 19:19:46 +0530

    restructure apollo client

--- apps/web-react/src/main.tsx
+++ apps/web-react/src/main.tsx
@@ -1,66 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import {
-  ApolloClient,
-  InMemoryCache,
-  ApolloProvider,
-  createHttpLink,
-  ApolloLink,
-} from "@apollo/client";
-import { setContext } from "@apollo/client/link/context";
-import { TokenRefreshLink } from "apollo-link-token-refresh";
-import jwtDecode from "jwt-decode";
+import { ApolloProvider } from "@apollo/client";
+
 import App from "./App";
-import { getAccessToken, setAccessToken } from "./accessToken";
-
-const httpLink = createHttpLink({
-  uri: "http://localhost:4000/graphql",
-  credentials: "include",
-});
-
-const authLink = setContext((_, { headers }) => {
-  const token = getAccessToken();
-
-  return {
-    headers: {
-      ...headers,
-      authorization: token ? `Bearer ${token}` : "",
-    },
-  };
-});
-
-// If the token is expired, we want to refresh it
-const refreshlink = new TokenRefreshLink({
-  accessTokenField: "accessToken",
-  isTokenValidOrUndefined: () => {
-    const token = getAccessToken();
-    if (!token) return true;
-
-    try {
-      const { exp } = jwtDecode<any>(token);
-      if (Date.now() >= exp * 1000) return false;
-      return true;
-    } catch (err) {
-      return false;
-    }
-  },
-  fetchAccessToken: (): Promise<Response> => {
-    return fetch("http://localhost:4000/refresh_token", {
-      credentials: "include",
-    });
-  },
-  handleFetch: (accessToken: string) => setAccessToken(accessToken),
-  // handleResponse? : (operation: Operation, accessTokenField) => {} // we can parse the response and handle it accordingly,
-  handleError: (err: Error) => {
-    console.error({ msg: err.message });
-    console.warn("Your refresh token is invalid. Try to relogin");
-  },
-});
-
-const client = new ApolloClient({
-  link: ApolloLink.from([refreshlink, authLink, httpLink]),
-  cache: new InMemoryCache(),
-});
+import client from "./ApolloClient";
 
 ReactDOM.render(
   <React.StrictMode>
